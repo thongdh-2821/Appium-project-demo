4431e606c79511450792b1adb0c723b6d9f262a3
Thong Do <do.hoang.thong@sun-asterisk.com>
2024-08-18 11:33:09 +0700

    laptop try to push code

--- src/main/java/com/example/AndroidFindElements.java
+++ src/main/java/com/example/AndroidFindElements.java
@@ -36,8 +36,6 @@ public class AndroidFindElements {
 
     //     WebElement myElementXpathShort = driver.findElement(AppiumBy.xpath("//*[@text=\"Accessibility\"]"));
     //     System.out.println("myElementXpathShort "+myElementXpathShort.getText());
-        //commit
-        System.out.println("a");
     }
     
 }
